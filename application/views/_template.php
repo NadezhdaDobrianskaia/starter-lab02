10064f83bb26608385ff13479a6f10cf6e6bfe4e
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-09-19 18:09:19 -0700

    correcting a misspelled word

--- application/views/_template.php
+++ application/views/_template.php
@@ -8,7 +8,7 @@ and open the template in the editor.
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>{pagetitile}</title>
+        <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
